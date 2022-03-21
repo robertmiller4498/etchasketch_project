09529f469d8568f3dc83922e1393011f975bb0f8
Rob <robertmiller4498@gmail.com>
2022-03-20 22:56:18 -0700

    getting close to having a working user input

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+// Coding with Rob helped with sizing of board- video on YouTube
+
+function populateBoard(size) {
 let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16 , 1fr)";
-board.style.gridTemplateRows = "repeat('16 , 1fr')";
+let squares = board.querySelectorAll("div");
+squares.forEach(div => div.remove());
+board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+};
 //256 is 16x16
 
+
+let amount = size * size;
 //This portion creates our grid 
-for(let i = 0; i < 256; i++) {
+for(let i = 0; i < amount; i++) {
  let square = document.createElement('div');
  square.classList.add("square");
  square.style.backgroundColor = "blue";
  board.insertAdjacentElement("beforeend", square);
- };
+ }
 
+
+populateBoard(16); 
+
+function changeSize(input) {
+    if(input >= 1 || <= 100) {
+        populateBoard(input);
+    } else {
+        return "Error! Input is not between 1-100";
+    }
+}
+
+
+//This portion makes the etch-a-sketch functional
 board.onmouseover = function(event) {
     let target = event.target;
     target.style.backgroundColor = "light grey";
-};
+}
 
 board.onmouseout = function(event) {
     let target = event.target;
     target.style.backgroundColor = "black";
-};
-
- 
+}
