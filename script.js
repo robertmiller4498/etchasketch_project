3052f6c42dc5b994ffae5749262797ff99170d88
Rob <robertmiller4498@gmail.com>
2022-03-20 20:30:42 -0700

    etch a sketch is working- took over a month but we did it!!

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ board.style.gridTemplateColumns = "repeat(16 , 1fr)";
 board.style.gridTemplateRows = "repeat('16 , 1fr')";
 //256 is 16x16
 
-//This portion creates our grid
- for(let i = 0; i < 256; i++) {
+//This portion creates our grid 
+for(let i = 0; i < 256; i++) {
  let square = document.createElement('div');
+ square.classList.add("square");
  square.style.backgroundColor = "blue";
  board.insertAdjacentElement("beforeend", square);
  };
 
- 
+board.onmouseover = function(event) {
+    let target = event.target;
+    target.style.backgroundColor = "light grey";
+};
 
- 
+board.onmouseout = function(event) {
+    let target = event.target;
+    target.style.backgroundColor = "black";
+};
 
  
