fdafbda4f7727a522c7ec3d7b9984d7938aa7f46
Rob <robertmiller4498@gmail.com>
2022-03-20 20:12:45 -0700

    pretty much reset my grid,  all 256 squares now appearing

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
-let board = document.querySelector('.board');
-board.style.gridTemplateColumns = ('16 , 1fr');
-board.style.gridTemplateRows = ('16 , 1fr');
-
-
+let board = document.querySelector(".board");
+board.style.gridTemplateColumns = "repeat(16 , 1fr)";
+board.style.gridTemplateRows = "repeat('16 , 1fr')";
 //256 is 16x16
+
 //This portion creates our grid
-for (let i = 0; i<256; i++) {
-let square = document.createElement('div');
-square.classList.add('square');
-square.style.backgroundColor = 'blue';
-board.insertAdjacentElement('beforeend', square);
-/* square[i].addEventListeners(mouseenter, mouseleave, function() {
-    if ('mouseenter') {
-        return square.style.backgroundColor = 'light grey';
-    } if ('mouseleave') {
-        return square.style.backgroundColor = 'black';
-    }
-}); */
-}
+ for(let i = 0; i < 256; i++) {
+ let square = document.createElement('div');
+ square.style.backgroundColor = "blue";
+ board.insertAdjacentElement("beforeend", square);
+ };
 
-//Mousehover work
-//Thought process: I need to develop an if/and statement in regards to what will happen when the mouse enters/exits each square.
-//This process is almost working, currently producing a syntax error. Try and bring them together more cohesively.
+ 
 
-/* for (let i = 0; i<256; i++) {
-    square[i].addEventListeners(mouseenter, funciton(), {
-        if (mouseenter) {
-            return square.style.backgroundColor = ''
-        }
-    });
-    square[i].addEventListeners(mouseleave, function() {
-        if (mouseleave) {
-            return square.style.backgroundColor = 'black'
-        }
-    }); */
+ 
 
+ 
