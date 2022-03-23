d6f7a3c20d8e124bdea24aa4fb3405078a67b70f
Rob <robertmiller4498@gmail.com>
2022-03-22 22:55:40 -0700

    Needs cleaning up- but prompt takes in and applies user input!!!

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-let populateBoard = (size) => {
-    let board = document.querySelector(".board");
-    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-     board.style.gridTemplateRows = `repeat('${size} , 1fr')`;
-    
-    let amount = value.onchange * value.onchange;
-     
-     for(let i = 0; i < amount; i++) {
-      let square = document.createElement('div');
-      square.classList.add("square");
-      square.style.backgroundColor = "blue";
-      board.insertAdjacentElement("beforeend", square);
-      };
-    
-    board.onmouseover = function(event) {
-        let target = event.target;
-        target.style.backgroundColor = "light grey";
-    };
-    
-    board.onmouseout = function(event) {
-        let target = event.target;
-        target.style.backgroundColor = "black";
-    };
-    
-   /*   let changeSize = () =>  {
-        if ((size >= 1) || (size <= 100)) {
-            return populateBoard;
-        } else {
-            return "Error! Please enter a number between 1 and 100.";
-        }
-    } */
-    };
-    
-    const btn = document.querySelector(".btn");
-    btn.addEventListener('click', (size) => {
-        return populateBoard;
-    });
+var userInput = window.prompt("What size grid would you like?");
 
+
+
+function createGrid(userInput) {
+let board = document.querySelector(".board");
+board.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+board.style.gridTemplateRows = `repeat('${userInput} , 1fr')`;
+    
+let amount = userInput * userInput;
+for(let i = 0; i < amount; i++) {
+    let square = document.createElement('div');
+    square.classList.add("square");
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+};
+    
+board.onmouseover = function(event) {
+    let target = event.target;
+    target.style.backgroundColor = "light grey";
+};
+    
+board.onmouseout = function(event) {
+    let target = event.target;
+    target.style.backgroundColor = "black";
+};
+
+};
+
+createGrid(userInput);
