a53b1e06c2afcca667fa3eb306e470bf6b8eb6b3
Rob <robertmiller4498@gmail.com>
2022-03-27 22:51:49 -0700

    Buttons and prompts functioning correctly

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var userInput = window.prompt("What size grid would you like?");
 
-
-
-function createGrid(userInput) {
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
-board.style.gridTemplateRows = `repeat('${userInput} , 1fr')`;
+ function createGrid(userInput) {
+ let board = document.querySelector(".board");
+ board.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+ board.style.gridTemplateRows = `repeat(${userInput} , 1fr)`;
     
 let amount = userInput * userInput;
 for(let i = 0; i < amount; i++) {
@@ -28,3 +26,41 @@ board.onmouseout = function(event) {
 };
 
 createGrid(userInput);
+
+//Function for button
+
+let form = document.getElementById("form");
+let boardReset = document.getElementsByClassName("board");
+form.addEventListener('reset', boardReset);
+function clearGrid () {
+/* var userInput = window.prompt("What size grid would you like?"); */
+
+ function createGrid(userInput) {
+ let board = document.querySelector(".board");
+ board.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+ board.style.gridTemplateRows = `repeat(${userInput} , 1fr)`;
+    
+let amount = userInput * userInput;
+for(let i = 0; i < amount; i++) {
+    let square = document.createElement('div');
+    square.classList.add("square");
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+};
+    
+board.onmouseover = function(event) {
+    let target = event.target;
+    target.style.backgroundColor = "light grey";
+};
+    
+board.onmouseout = function(event) {
+    let target = event.target;
+    target.style.backgroundColor = "black";
+};
+
+};
+
+createGrid(userInput);
+};
+
+
